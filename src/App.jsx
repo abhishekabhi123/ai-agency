8f66232bfdda93879d70ae4ee7096d7f9e11ad39
abhishekabhi123 <vadakkemadamabhishek@gmail.com>
2025-09-30 09:43:34 +0530

    footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
@@ -6,13 +6,31 @@ import Services from "./components/Services";
 import OurWork from "./components/OurWork";
 import Teams from "./components/Teams";
 import ContactUs from "./components/ContactUs";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
+
+  const dotRef = useRef(null);
+  const outlineRef = useRef(null);
+
+  const mouse = useRef({ x: 0, y: 0 });
+  const position = useRef({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMove = (e) => {
+      mouse.current.x = e.clientX;
+      mouse.current.y = e.clientY;
+    };
+    document.addEventListener("mousemove", handleMove);
+  }, []);
+
   return (
     <div className="dark:bg-black relative">
+      <Toaster />
       <NavBar theme={theme} setTheme={setTheme} />
       <Hero />
       <TrustedBy />
@@ -20,6 +38,16 @@ const App = () => {
       <OurWork />
       <Teams />
       <ContactUs />
+      <Footer theme={theme} />
+      {/* // mouse pointer */}
+      <div
+        ref={outlineRef}
+        className="fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]"
+      ></div>
+      <div
+        ref={dotRef}
+        className="fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]"
+      ></div>
     </div>
   );
 };
